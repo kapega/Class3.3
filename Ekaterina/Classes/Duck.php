4d3233d10f556d26107ac7bcf5fa82d2c08329ef
kapega <37337675+kapega@users.noreply.github.com>
2018-04-18 23:40:06 +0300

    18-04

--- Ekaterina/Classes/Duck.php
+++ Ekaterina/Classes/Duck.php
@@ -2,30 +2,30 @@
 
 namespace Ekaterina\Classes;
 
-class Duck extends Product implements EkaterinaInterfacesDuckable
+class Duck extends Product implements \Ekaterina\Interfaces\Duckable
 {
     public $color;
     public $size;
-	
+    
     public function __construct($name, $color, $size)
     {
         $this->name = $name;
         $this->color = $color;
         $this->size = $size;
     }
-	
+    
     public function toString()
     {
-        return "Это {$this->name}, цвет {$this->color} и размер {$this->size}.<br />";
+        return "Это {$this->name}, цвет {$this->color} и размер {$this->size}.<br>";
     }
-	
+    
     public function checkUgly()
     {
         if ($this->color == 'белый' && $this->size == 'big') {
-            return 'Это же Гадкий утёнок!<br />';
+            return 'Это же Гадкий утёнок!<br>';
         }
         else {
-            return "Этот {$this->color} утёнок нормально выглядит. <br />";
+            return "Этот {$this->color} утёнок нормально выглядит. <br>";
         }
     }
 }
--- Ekaterina/Classes/Product.php
+++ Ekaterina/Classes/Product.php
@@ -2,27 +2,27 @@
 
 namespace Ekaterina\Classes;
 
-class Product extends ClassWithPrice implements EkaterinaInterfacesHasAmount
+class Product extends ClassWithPrice implements \Ekaterina\Interfaces\HasAmount // Товар
 {
     public $amount;
-
+    
     public function __construct($name, $amount, $price)
     {
         $this->name = $name;
         $this->amount = $amount;
         $this->price = $price;
     }
-	
+    
     public function toString()
     {
-        return "Это {$this->name}, {$this->amount} штук по цене {$this->price}.<br />";
+        return "Это {$this->name}, {$this->amount} штук по цене {$this->price}.<br>";
     }
-	
+    
     public function getAmount()
     {
         return $this->amount;
     }
-	
+    
     public function getPrice()
     {
         if ($this->amount < 10) {
@@ -35,10 +35,10 @@ class Product extends ClassWithPrice implements EkaterinaInterfacesHasAmount
             return $this->price;
         }
     }
-	
+    
     public function getPriceText()
     {
         $str = "Стоимость билета по направлению <strong>{$this->name}</strong> на данный момент равна ";
-        return $str . $this->getPrice() . '.<br />';
+        return $str . $this->getPrice() . '.<br>';
     }
 }
